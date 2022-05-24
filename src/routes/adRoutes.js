dc33513131a334c01ab5357f65b5bfa41bbf7344
Dominykas-Zernys <zernysdominykas@gmail.com>
2022-05-24 23:44:28 +0300

    new advert route changed

--- src/routes/adRoutes.js
+++ src/routes/adRoutes.js
@@ -1,17 +1,12 @@
+/* eslint-disable object-curly-newline */
 const express = require('express');
-const {
-  getAds,
-  getOwnerAds,
-  getAd,
-  postAd,
-  deleteAd,
-} = require('../controllers/adController');
+const { getAds, getOwnerAds, getAd, postAd, deleteAd } = require('../controllers/adController');
 const { verifyJWToken, validateAdvert } = require('../helpers');
 
 const adRouter = express();
 
 adRouter.get('/', getAds);
-adRouter.get('/user-ads/', verifyJWToken, getOwnerAds);
+adRouter.get('/user-ads', verifyJWToken, getOwnerAds);
 adRouter.get('/:id', getAd);
 adRouter.post('/', verifyJWToken, validateAdvert, postAd);
 adRouter.delete('/:id', verifyJWToken, deleteAd);
